2803ebb5d425208ffca0b9e9580f00a3cec04cc4
Alaa <lolaam021@gmail.com>
2024-09-26 05:26:08 +0300

    Add project files.

---
+++ Store.Web/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.Services.ProductServices;
+using Store.Services.Services.ProductServices.Dto;
+
+namespace Store.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductServices _productServices;
+
+        public ProductsController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<BrandsTypesDetailsDto>>> GetAllBrands()
+            => Ok(await _productServices.GetAllBrandsAsync());
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<BrandsTypesDetailsDto>>> GetAllTypes()
+            => Ok(await _productServices.GetAllTypesAsync());
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductDetailsDto>>> GetAllProducts()
+           => Ok(await _productServices.GetAllProductsAsync());
+
+        [HttpGet]
+        public async Task<ActionResult<ProductDetailsDto>> GetProductById(int? id)
+           => Ok(await _productServices.GetProductByIdAsync(id));
+    }
+}
