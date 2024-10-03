72a361f0781c7f45188427b3da24f71928d6ebc2
Alaa <lolaam021@gmail.com>
2024-10-03 10:53:16 +0300

    commit 3

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -7,6 +7,14 @@ using Store.Repository.Interfaces;
 using Store.Repository.Repositories;
 using Store.Services.Services.ProductServices.Dto;
 using Store.Services.Services.ProductServices;
+using Store.Web.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.HandleResponses;
+using Store.Web.Extensions;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 namespace Store.Web
 {
     public class Program
@@ -24,9 +32,16 @@ namespace Store.Web
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
-            builder.Services.AddScoped<IProductServices , ProductServices>();
-            builder.Services.AddAutoMapper(typeof(ProductProfile));
+            builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+            {
+                //var configurations = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"));
+                //return ConnectionMultiplexer.Connect(configurations);
+
+                return ConnectionMultiplexer.Connect(ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis")));
+            });
+
+            builder.Services.AddApplicationServices();
+
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -42,6 +57,8 @@ namespace Store.Web
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
